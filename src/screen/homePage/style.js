e8fcda6a32150c8167ac410a5d1b2f4f35504c9d
riyazthk <er.riyaz2507@gmail.com>
2020-10-11 20:34:03 +0530

    [Riyaz]ADDED:To update a landscape mode

--- src/screen/homePage/style.js
+++ src/screen/homePage/style.js
@@ -3,7 +3,7 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   body: {
-    height: '100%',
+     height: '100%',
     backgroundColor: 'white',
   },
   headerSectionContainer: {
